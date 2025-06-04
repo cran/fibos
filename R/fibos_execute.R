47a3a3220ade90b705aca4b9408ffaa29a30209f
Herson Soares <hersonhebert@hotmail.com>
2025-06-04 00:00:02 +0000

    version 2.0.1

--- R/fibos_execute.R
+++ R/fibos_execute.R
@@ -3,6 +3,7 @@
 #' 
 #' @param pdb 4-digit PDB id (will fetch it from the RCSB repository) or the path to a PDB local file.
 #' @param method Method to be used: 'OS' (classic) or 'FIBOS' (default). The classic 'OS' covers the surface radially with one of the axes as a reference when allocating the dots. In 'FIBOS', Fibonacci spirals were used to allocate the dots, which is known to produce lower axial anisotropy as well as more evenly spaced points on a sphere.
+#' @param density_dots Distribution density of atomic dots for surface occlusion calculation.
 #'
 #' @description The function executes the implemented methods.
 #'              Using this function, it is possible to calculate occluded areas
@@ -24,15 +25,9 @@
 #' @author Joao Paulo Roquim Romanelli (joaoromanelli@unifei.edu.br)
 #' @author Patrick Fleming (Pat.Fleming@jhu.edu)
 #'
-execute = function(pdb, method){
-  sys_info = Sys.info()
-  if(sys_info["sysname"] == "Windows"){
-    python_code = glue::glue("import fibos;fibos.occluded_surface('{pdb}','{method}')")
-    reticulate::py_run_string(python_code)
-  }else{
+execute = function(pdb, method, density_dots){
     python = reticulate::import("fibos", delay_load = TRUE)
-    python$occluded_surface(pdb, method)
-  }
+    dados = python$occluded_surface(pdb, method, density_dots)
   if (tolower(fs::path_ext(pdb)) == "pdb") {
     pdb = fs::path_ext_remove(pdb)
   }
@@ -63,14 +58,8 @@ execute = function(pdb, method){
 #' @author Patrick Fleming (Pat.Fleming@jhu.edu)
 
 osp_internal = function(file){
-  sys_info = Sys.info()
-  if(sys_info["sysname"] == "Windows"){
-    python_code = glue::glue("import fibos;fibos.osp('{file}')")
-    reticulate::py_run_string(python_code)
-  }else{
-    python = reticulate::import("fibos", delay_load = TRUE)
-    result = python$osp(file)
-  }
+  python = reticulate::import("fibos", delay_load = TRUE)
+  result = python$osp(file)
   file = fs::path_ext_remove(file)
   file = fs::path_ext_set(file,"pak")
   return(read_osp(file))
